6d02c472e673d57f492914233414ff69beb8d2a3
Michael Mayer <39913724+dwaltsch@users.noreply.github.com>
2021-09-30 20:47:17 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,11 +7,17 @@ sudo apt install -y python3-pip
 sudo apt install -y python3
 sudo apt install -y neofetch
 sudo apt install -y docker
+sudo apt install -y nginx
+sudo apt install -y gcc
+sudo apt install -y language-pack-ru
+#Config
+update-locale LANG=de_DE.UTF-8
 #Security
 #Test
 speedtest
 python3 -V
-docker -v
+docker --version
+locale
 neofetch
 #Clean up
 rm -r Hetznersetup
