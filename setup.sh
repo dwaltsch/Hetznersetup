8176f66247e4f003e62943e36e23c86eb5284a22
Michael Mayer <39913724+dwaltsch@users.noreply.github.com>
2021-09-30 20:08:09 +0200

    rf

--- setup.sh
+++ setup.sh
@@ -1,20 +1,17 @@
 #Update
 sudo apt update -y
 sudo apt upgrade -y
-
 #Install
 sudo apt install -y speedtest-cli
 sudo apt install -y python3-pip
 sudo apt install -y python3
 sudo apt install -y neofetch
-
+sudo apt install -y docker
 #Security
-
-
 #Test
 speedtest
 python3 -V
+docker -v
 neofetch
-
 #Clean up
 rm -r Hetznersetup
