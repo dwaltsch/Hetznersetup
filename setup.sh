4cde8534b35009e7aea7d4701241e77f5038f3db
Michael Mayer <39913724+dwaltsch@users.noreply.github.com>
2021-09-30 20:05:12 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,20 @@
+#Update
 sudo apt update -y
 sudo apt upgrade -y
 
+#Install
+sudo apt install -y speedtest-cli
+sudo apt install -y python3-pip
+sudo apt install -y python3
+sudo apt install -y neofetch
+
+#Security
+
+
+#Test
+speedtest
+python3 -V
+neofetch
+
+#Clean up
+rm -r Hetznersetup
