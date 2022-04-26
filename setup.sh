ed1a3ba6a604480ea0ca1d95f4e9b9fcdac8fc83
Michael Mayer <39913724+dwaltsch@users.noreply.github.com>
2022-04-26 21:11:22 +0200

    Docker

--- setup.sh
+++ setup.sh
@@ -6,8 +6,23 @@ sudo apt install -y speedtest-cli
 sudo apt install -y python3-pip
 sudo apt install -y python3
 sudo apt install -y neofetch
-sudo apt install -y docker
-sudo apt install -y docker-compose
+#Docker
+sudo apt install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update -y
+sudo apt install -ydocker-ce docker-ce-cli containerd.io docker-compose-plugin
+#Docker Compose
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.4.1/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
 #Config
 update-locale LANG=de_DE.UTF-8
 sudo timedatectl set-timezone Europe/Vienna
