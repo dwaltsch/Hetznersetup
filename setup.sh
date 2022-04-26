50882b18a498799065a50db9d22575b641612e69
Michael Mayer <39913724+dwaltsch@users.noreply.github.com>
2022-04-26 20:59:51 +0200

    rein und raus

--- setup.sh
+++ setup.sh
@@ -7,22 +7,20 @@ sudo apt install -y python3-pip
 sudo apt install -y python3
 sudo apt install -y neofetch
 sudo apt install -y docker
-sudo apt install -y nginx
-sudo apt install -y gcc
-sudo apt install -y language-pack-ru
-sudo apt-key adv -y --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
-sudo add-apt-repository -y 'deb [arch=amd64] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse'
-sudo apt install -y mongodb-org
+sudo apt install -y docker-compose
 #Config
 update-locale LANG=de_DE.UTF-8
-sudo systemctl start mongod
-sudo systemctl enable mongod
+sudo timedatectl set-timezone Europe/Vienna
 #Security
+sudo apt install -y language-pack-ru
+sudo apt install -y fail2ban
+#needs aditional config
 #Test
 speedtest
 python3 -V
 docker --version
 locale
+timedatectl
 neofetch
 #Clean up
 rm -r Hetznersetup
